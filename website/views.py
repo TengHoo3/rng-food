7af197054eb524eaa61e2cc22253a54fceabcc1b
TengHoo <tenghoo3@gmail.com>
2021-12-03 19:33:07 +0800

    remove trash

--- website/views.py
+++ website/views.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Cuisine, Store
 from . import db
-import json
 
 views = Blueprint('views', __name__)
 
@@ -16,8 +15,6 @@ def home():
         cuisine_type = request.form.get('cuisine_type')
         if cuisine_type == 0:
             cuisine_type = 'None'
-        # print('cuisine : ', type(cuisine))
-        # print('store : ',type(store))
 
         if len(cuisine) < 1:
             pass 
@@ -36,16 +33,3 @@ def home():
             flash('Store added!', category='success')
 
     return render_template("home.html", user=current_user)
-
-
-# @views.route('/delete-note', methods=['POST'])
-# def delete_note():
-#     note = json.loads(request.data)
-#     noteId = note['noteId']
-#     note = Note.query.get(noteId)
-#     if note:
-#         if note.user_id == current_user.id:
-#             db.session.delete(note)
-#             db.session.commit()
-
-#     return jsonify({})
